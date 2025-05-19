5a333dd029ddde600317eaa478e16a93c9a02f6d
Kaushal <90102364+Itsmekaushal@users.noreply.github.com>
2025-05-19 14:00:28 +0530

    Update app.module.ts

--- MapMyCity/src/app/app.module.ts
+++ MapMyCity/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { GoogleMapsModule } from '@angular/google-maps';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, GoogleMapsModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
